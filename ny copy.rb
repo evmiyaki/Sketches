e108022670267e26bb793b515bdedb75c9835908
Evan Miyaki <emiyaki@CART115-LAP03.local>
2014-02-03 12:59:22 -1000

    Sketches
    New year sketches

---
+++ ny copy.rb
@@ -0,0 +1,7 @@
+size 300, 300
+ellipse 10, 10, 20, 20
+ellipse 50, 50, 40, 40
+ellipse 90, 90, 60, 60
+ellipse 130, 130, 80, 80
+ellipse 170, 170, 100, 100
+ellipse 210, 210, 120, 120
---
+++ victa copy.rb
@@ -0,0 +1,17 @@
+size 300, 300
+fill 244
+triangle 0, 50, 100, 50, 50, 0
+fill 200
+triangle 0, 100, 100, 100, 50, 50
+fill 150
+triangle 0, 150, 100, 150, 50, 100
+fill 100
+triangle 0, 200, 100, 200, 50, 150
+fill 244
+triangle 100, 50, 200, 50, 150, 0
+fill 200
+triangle 100, 100, 200, 100, 150, 50
+fill 150
+triangle 100, 150, 200, 150, 150, 100
+fill 100
+triangle 100, 200, 200, 200, 150, 150
